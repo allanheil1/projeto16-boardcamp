45195eac95e8f73cdc930649ccc310afc20231c9
Allan de Melo Heil <allanmheil@hotmail.com>
2023-02-09 13:22:06 -0300

    created validation empty functions

---
+++ src/middlewares/customerValidation.js
@@ -0,0 +1,8 @@
+import connection from '../database.js';
+import customerSchema from '../schemas/customerSchema.js';
+
+async function validateCustomer(req, res, next){
+
+}
+
+export { validateCustomer };
---
+++ src/middlewares/rentalValidation.js
@@ -0,0 +1,12 @@
+import connection from '../database.js';
+import rentalSchema from '../schemas/rentalSchema.js';
+
+async function validateRental(req, res, next){
+    
+}
+
+async function validateRentalId(req, res, next){
+    
+}
+
+export { validateRental, validateRentalId };
