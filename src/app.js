aea5d74edd679373e66ab088946369212a4398bb
allanheil1 <allanheil1@gmail.com>
2023-02-10 18:45:41 -0300

    added routers in app.js

--- src/app.js
+++ src/app.js
@@ -3,6 +3,8 @@ import cors from 'cors';
 import dotenv from 'dotenv';
 
 import gameRouter from './routers/gameRouter.js';
+import customerRouter from './routers/customerRouter.js';
+import rentalRouter from './routers/rentalRouter.js';
 
 dotenv.config();
 
@@ -10,6 +12,8 @@ const app = express();
 app.use(json());
 app.use(cors());
 app.use(gameRouter);
+app.use(customerRouter);
+app.use(rentalRouter);
 
 
 app.listen(process.env.PORT, () => { 
